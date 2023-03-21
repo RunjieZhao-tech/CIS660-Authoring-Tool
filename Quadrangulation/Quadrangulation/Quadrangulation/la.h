d913fffa72a37ef786b6bf2db1d01ec21161b1a2
cao <licheng_cao@126.com>
2023-03-21 19:23:12 -0400

    add glm lib, add HalfEdges

---
+++ Quadrangulation/Quadrangulation/Quadrangulation/la.h
@@ -0,0 +1,11 @@
+#pragma once
+// Primary GLM library
+#include <glm/glm.hpp>
+// For glm::translate, glm::rotate, and glm::scale.
+#include <glm/gtc/matrix_transform.hpp>
+// For glm::to_string.
+#include <glm/gtx/string_cast.hpp>
+// For glm::value_ptr.
+#include <glm/gtc/type_ptr.hpp>
+
+#include <vector>
