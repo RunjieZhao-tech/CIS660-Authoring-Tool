f6948e74c5cc1506157aa3015c8b277c9300c943
cao <licheng_cao@126.com>
2023-03-30 18:55:26 -0400

    tile solver but tile not defined

--- Quadrangulation/Quadrangulation/Quadrangulation/tile_solver.cpp
+++ Quadrangulation/Quadrangulation/Quadrangulation/tile_solver.cpp
@@ -1,4 +1,109 @@
 #include "tile_solver.h"
+
+
+std::vector<std::vector<Face*>> LTile::possibleTile(const std::vector<Face*>& quads)const {
+
+}
+
+std::vector<std::vector<Face*>> SquareTile::possibleTile(const std::vector<Face*>& quads)const {
+
+}
+
+std::vector<std::vector<Face*>> L3Tile::possibleTile(const std::vector<Face*>& quads)const {
+
+}
+
+std::vector<std::vector<Face*>> L2Tile::possibleTile(const std::vector<Face*>& quads)const {
+
+}
+
+std::vector<std::vector<Face*>> TileSolver::solveTiling(const std::vector<Face*>& quads, std::vector<Tile*>tiles)const {
+	lprec* lp;
+	int Ncol = 0;//number of all possible tiling
+	int* T = nullptr;
+	REAL* weights = nullptr;
+
+	//build a map refer to all quads in a patch
+	std::unordered_map<Face*, int> quadmap;
+	for (int i = 0;i < quads.size();i++) {
+		quadmap[quads[i]] = i + 1;
+	}
+	//build the linear problem solver
+	lp = make_lp(quads.size(), 0);
+	if (lp == NULL) {
+		std::cout << "make linear solver failed" << std::endl;
+	}
+
+	//for each tiles, find its possible tiling and build a column for it
+	int colIndex = 0;//to find which col is currently set
+	for (Tile* tile : tiles) {
+		std::vector<std::vector<Face*>> tileChoice = tile->possibleTile(quads);
+		for (int i = 0;i < tileChoice.size();i++) {
+			std::vector<int> rowNumber; // row that has T for this tiling choice
+			//for the maximize function
+			rowNumber.push_back(0);//the objective function is always the first row
+			std::vector<double> colFactor;
+			colFactor.push_back(tile->weight);
+			//describe quads(fk in the paper) that is going to be filled with T
+			//this is used for constraint sum(T) <= 1
+			for (int j = 0;j < tileChoice[i].size();j++) {
+				//find index of quad
+				auto itr = quadmap.find((tileChoice[i][j]));
+				rowNumber.push_back(itr->second);//describe which face is affected
+				colFactor.push_back(1);//since sum(T) is what we want, to make sure the size of colFactor and rowNumber stay the same
+			}
+			add_columnex(lp, rowNumber.size(), &colFactor[0], &rowNumber[0]);
+			set_binary(lp, colIndex+1, TRUE);
+			colIndex++;
+		}
+	}
+
+	//for each fk in M, sum(T) should be 1;
+	int totalColNum = get_Ncolumns(lp);
+	std::vector<double> rowVal;
+	rowVal.reserve(totalColNum + 1);//allocate memory
+	
+	for (int i = 0;i < quads.size();i++) {
+		rowVal.erase(rowVal.begin(), rowVal.begin()+ totalColNum + 1);
+		get_row(lp, i + 1, &rowVal[0]);
+		add_constraint(lp, &rowVal[0], EQ /*LE*/, 1);
+	}
+
+	//we want to maximize sum(W*T)
+	rowVal.erase(rowVal.begin(), rowVal.begin() + totalColNum + 1);
+	get_row(lp, 0, &rowVal[0]);
+	set_obj_fn(lp, &rowVal[0]);
+	set_maxim(lp);
+	solve(lp);
+	get_variables(lp, &rowVal[0]);//get result tiling
+	std::vector<std::vector<Face*>> result;
+	//allocate memory
+	std::vector<int> rowId;
+	rowId.reserve(quads.size() + 1);
+	std::vector<double> colValue;
+	colValue.reserve(quads.size() + 1);//actually useless since it's a binary value
+	//find valid tile choice convert it to vector<Face*> used in result
+	for (int i = 0;i < totalColNum;i++) {
+		if (rowVal[i] == 0)continue;//this tiling choice is not selected;
+		//clean vector to store new information
+		rowId.erase(rowId.begin(), rowId.begin() + quads.size() + 1);
+		colValue.erase(colValue.begin(), colValue.begin() + quads.size() + 1);
+		
+		//for each col find the tiledface described by the column
+		std::vector<Face*> tiledFace;
+		get_columnex(lp, i + 1, &colValue[0], &rowId[0]);
+		for (int i = 0;i < quads.size() + 1;i++) {
+			if (rowId[i] == 0) {
+				if (colValue[i] == 0)break;//useless value
+				continue;//objective row
+			}
+			Face* tiledQuad = quads[rowId[i] - 1];//because we store quad id begins with 1
+			tiledFace.push_back(tiledQuad);
+		}
+		result.push_back(tiledFace);
+	}
+	return result;
+}
 //int demo()
 //{
 //    lprec* lp;
--- Quadrangulation/Quadrangulation/Quadrangulation/tile_solver.h
+++ Quadrangulation/Quadrangulation/Quadrangulation/tile_solver.h
@@ -1,3 +1,41 @@
 #pragma once
 #include"lp_solve/lp_lib.h"
+#include "half_edge.h"
+#include <iostream>
 #pragma comment(lib, "lp_solve/lpsolve55.lib")
+class Tile {
+public:
+	float weight = 4;
+	Tile() {};
+	//iterate all possible filing for a certain tile
+	virtual std::vector<std::vector<Face*>> possibleTile(const std::vector<Face*>& quads)const;
+};
+
+//tile[]
+//    []
+//    [][]
+class LTile: public Tile {
+	std::vector<std::vector<Face*>> possibleTile(const std::vector<Face*>& quads)const override;
+};
+
+//tile[][]
+//    [][]
+class SquareTile : public Tile {
+	std::vector<std::vector<Face*>> possibleTile(const std::vector<Face*>& quads)const override;
+};
+
+//tile: [][][]
+class L3Tile : public Tile {
+	std::vector<std::vector<Face*>> possibleTile(const std::vector<Face*>& quads)const override;
+};
+
+//tile: [][]
+class L2Tile : public Tile {
+	std::vector<std::vector<Face*>> possibleTile(const std::vector<Face*>& quads)const override;
+};
+
+class TileSolver {
+public:
+	TileSolver();
+	std::vector<std::vector<Face*>> solveTiling(const std::vector<Face*>& quads, std::vector<Tile*>tiles)const;
+};
